8841236f6e188773661dc35d2ec2af61266507ee
RynoRube <kathrynrubert@u.boisestate.edu>
2025-05-23 22:54:32 -0600

    Finished Activity

--- FormatChecker.java
+++ FormatChecker.java
@@ -20,120 +20,137 @@ public class FormatChecker {
     private boolean lineFailure; // determine if a line has incorrect data types
 
     public FormatChecker(String[] args) throws FileNotFoundException {
-        if (args.length == 1 && args[0] != null) {
-            String filename = args[0];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] != null) {
+                String filename = args[i];
 
-            Scanner fileScanner = new Scanner(new File(filename));
+                Scanner fileScanner = new Scanner(new File(filename));
 
-            int lineCounter = 0;
+                int lineCounter = 0;
 
-            while (fileScanner.hasNext() && firstLineFailure != true && lineFailure != true) {
+                while (fileScanner.hasNext() && firstLineFailure != true && lineFailure != true) {
 
-                String line = fileScanner.nextLine();
+                    String line = fileScanner.nextLine();
 
-                // Scanner lineScanner = new Scanner(line);
+                    // Scanner lineScanner = new Scanner(line);
 
-                // lineScanner.useDelimiter(" ");
+                    // lineScanner.useDelimiter(" ");
 
-                // loop through entries in the line to determine how many elements there are
-                // int elementCounter = 0;
-                // while(lineScanner.hasNext())
-                // {
-                // elementCounter += 1;
+                    // loop through entries in the line to determine how many elements there are
+                    // int elementCounter = 0;
+                    // while(lineScanner.hasNext())
+                    // {
+                    // elementCounter += 1;
 
-                // }
+                    // }
 
-                String[] elements = line.split("\\s+"); // This is another option instead of the delimiter
+                    String[] elements = line.split("\\s+"); // This is another option instead of the delimiter
 
-                int elementCounter = elements.length;
+                    int elementCounter = elements.length;
 
-                // variable to count the number of rows
-                if (lineCounter == 0) {
-                    // if(loop result != 2) {say it's invalid and stop the while loop}
-                    if (elementCounter != 2) {
-                        System.out.println(
-                                "Invalid file. Too many arguments on the first line. Cannot tell how many rows and columns are anticipated.");
-                        firstLineFailure = true;
+                    // variable to count the number of rows
+                    if (lineCounter == 0) {
+                        // if(loop result != 2) {say it's invalid and stop the while loop}
+                        if (elementCounter != 2) {
+                            System.out.println(
+                                    "Invalid file. Too many arguments on the first line. Cannot tell how many rows and columns are anticipated.");
+                            firstLineFailure = true;
 
+                        }
+                        // else {store values in integer variables and do more tests}
+                        else {
+                            try {
+                                statedRows = Integer.parseInt(elements[0]);
+                                statedColumns = Integer.parseInt(elements[1]);
+                                matrixFromFile = new double[statedRows][statedColumns];
+                            } catch (NumberFormatException e) {
+                                System.out.println("Invalid file. The values on the first line must both be integers.");
+                                firstLineFailure = true;
+                            }
+                            if (statedRows <= 0 || statedColumns <= 0) {
+                                System.out.println("Invalid file. The values on the first line must both be positive.");
+                                firstLineFailure = true;
+                            }
+
+                        }
                     }
-                    // else {store values in integer variables and do more tests}
+
                     else {
-                        try {
-                            statedRows = Integer.parseInt(elements[0]);
-                            statedColumns = Integer.parseInt(elements[1]);
-                            matrixFromFile = new double[statedRows][statedColumns];
-                        } catch (NumberFormatException e) {
-                            System.out.println("Invalid file. The values on the first line must both be integers.");
-                            firstLineFailure = true;
-                        }
-                        if (statedRows <= 0 || statedColumns <= 0) {
-                            System.out.println("Invalid file. The values on the first line must both be positive.");
-                            firstLineFailure = true;
-                        }
+                        if (lineCounter <= statedRows && elementCounter == statedColumns) {
+                            for (int j = 0; j < elementCounter; j++) {
+                                String currentElement = elements[j];
+                                try {
+                                    double matrixElement = Double.parseDouble(currentElement);
+                                    matrixFromFile[lineCounter - 1][j] = matrixElement;
+                                } catch (NumberFormatException e) {
+                                    System.out.println("Invalid file. Not all data is the correct type.");
+                                    lineFailure = true;
+                                }
+                            }
+                        } else if (lineCounter > statedRows && elementCounter == statedColumns) {
+                            System.out.println("Invalid file. Too many rows.");
+                            lineFailure = true;
 
-                    }
-                }
-
-                else {
-                    for (int i = 0; i < elementCounter; i++) {
-                        String currentElement = elements[i];
-                        try {
-                            double matrixElement = Double.parseDouble(currentElement);
-                            matrixFromFile[lineCounter - 1][i] = matrixElement;
-                        } catch (NumberFormatException e) {
-                            System.out.println("Invalid file. Not all data is the correct type.");
+                        } else if (lineCounter > statedRows && elementCounter < statedColumns) {
+                            System.out.println("Invalid file. Too many rows and not enough columns.");
+                            lineFailure = true;
+                        } else if (lineCounter > statedRows && elementCounter > statedColumns) {
+                            System.out.println("Invalid file. Too many rows and too many columns.");
+                            lineFailure = true;
+                        } else if (lineCounter <= statedRows && elementCounter < statedColumns) {
+                            System.out.println("Invalid file. Too few columns");
+                            lineFailure = true;
+                        } else if (lineCounter <= statedRows && elementCounter > statedColumns) {
+                            System.out.println("Invalid file. Too many columns.");
                             lineFailure = true;
                         }
                     }
+
+                    lineCounter++;
+
+                    // lineScanner.close();
                 }
 
-                lineCounter++;
+                fileScanner.close();
 
-                // lineScanner.close();
-            }
+                // look to see if the number of rows and columns are correct
+                // determine if file fails or not
+                if (firstLineFailure != true && lineFailure != true) {
+                    givenColumns = matrixFromFile[0].length;
+                    givenRows = matrixFromFile.length;
 
-            fileScanner.close();
-
-            // look to see if the number of rows and columns are correct
-            // determine if file fails or not
-            if (firstLineFailure != true && lineFailure != true) {
-                givenColumns = matrixFromFile[0].length;
-                givenRows = matrixFromFile.length;
-
-                if (statedColumns == givenColumns && statedRows == givenRows && lineFailure != true
-                        && firstLineFailure != true) {
-                    System.out.println("Valid file!");
-                    for (int i = 0; i < matrixFromFile.length; i++) {
-                        for (int j = 0; j < matrixFromFile[0].length; j++) {
-                            System.out.print(matrixFromFile[i][j] + " ");
+                    if (statedColumns == givenColumns && statedRows == givenRows && lineFailure != true
+                            && firstLineFailure != true) {
+                        System.out.println("Valid file!");
+                        for (int k = 0; k < matrixFromFile.length; k++) {
+                            for (int j = 0; j < matrixFromFile[0].length; j++) {
+                                System.out.print(matrixFromFile[k][j] + " ");
+                            }
+                            System.out.println("");
                         }
-                        System.out.println("");
-                    }
-                } else {
-                    if (statedColumns == givenColumns && statedRows < givenRows) {
-                        System.out.println("Invalid file. Too many rows.");
-                    } else if (statedColumns == givenColumns && statedRows > givenRows) {
-                        System.out.println("Invalid file. Not enough rows.");
-                    } else if (statedColumns < givenColumns && statedRows == givenRows) {
-                        System.out.println("Invalid file. Too many columns.");
-                    } else if (statedColumns > givenColumns && statedRows == givenRows) {
-                        System.out.println("Invalid file. Not enough columns.");
-                    } else if (statedColumns < givenColumns && statedRows < givenRows) {
-                        System.out.println("Invalid file. Too many columns and too many rows.");
-                    } else if (statedColumns < givenColumns && statedRows > givenRows) {
-                        System.out.println("Invalid file. Too many columns and not enough rows.");
-                    } else if (statedColumns > givenColumns && statedRows < givenRows) {
-                        System.out.println("Invalid file. Not enough columns and too many rows.");
-                    } else if (statedColumns > givenColumns && statedRows > givenRows) {
-                        System.out.println("Invalid file. Not enough columns and not enough rows.");
+                    } else {
+                        if (statedColumns == givenColumns && statedRows < givenRows) {
+                            System.out.println("Invalid file. Too many rows.");
+                        } else if (statedColumns == givenColumns && statedRows > givenRows) {
+                            System.out.println("Invalid file. Not enough rows.");
+                        } else if (statedColumns < givenColumns && statedRows == givenRows) {
+                            System.out.println("Invalid file. Too many columns.");
+                        } else if (statedColumns > givenColumns && statedRows == givenRows) {
+                            System.out.println("Invalid file. Not enough columns.");
+                        } else if (statedColumns < givenColumns && statedRows < givenRows) {
+                            System.out.println("Invalid file. Too many columns and too many rows.");
+                        } else if (statedColumns < givenColumns && statedRows > givenRows) {
+                            System.out.println("Invalid file. Too many columns and not enough rows.");
+                        } else if (statedColumns > givenColumns && statedRows < givenRows) {
+                            System.out.println("Invalid file. Not enough columns and too many rows.");
+                        } else if (statedColumns > givenColumns && statedRows > givenRows) {
+                            System.out.println("Invalid file. Not enough columns and not enough rows.");
+                        }
                     }
                 }
+            } else {
+                throw new FileNotFoundException();
             }
-
-        }
-
-        else {
-            throw new FileNotFoundException();
         }
 
     }
